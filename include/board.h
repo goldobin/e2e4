e3801fa24b4765ea2b5e492ef979b9626628da5c
Oleksandr Goldobin <o.goldobin@shellrecharge.com>
2025-09-02 11:59:17 +0200

    Check if king threatened and/or can move

--- include/board.h
+++ include/board.h
@@ -13,6 +13,7 @@ constexpr size_t POS_STR_LEN     = 2;
 constexpr size_t MOVE_STR_LEN    = 2 * POS_STR_LEN;
 constexpr size_t THREATS_CAP     = 16;
 constexpr size_t PIECE_TYPES_CAP = 20;
+constexpr size_t POSITIONS_CAP   = 8;
 
 typedef enum {
     PIECE_TYPE_UNSPECIFIED = 0,
@@ -39,6 +40,11 @@ typedef struct {
     size_t col;
     size_t row;
 } Pos;
+
+typedef struct {
+    Pos    arr[POSITIONS_CAP];
+    size_t len;
+} Positions;
 
 typedef struct {
     int x;
@@ -97,6 +103,9 @@ typedef struct {
 bool       Pos_Equals(Pos a, Pos b);
 bool       Pos_IsValid(Pos a);
 Side       Pos_BoardSide(Pos a);
+bool       Pos_Find(Pos* dst, const Squares src, PieceType t, Side s);
+void       Positions_Around(Positions* dst, Pos p);
+void       Positions_Append(Positions* dst, Pos p);
 bool       PieceType_IsValid(PieceType t);
 bool       PieceTypes_Equals(PieceTypes a, PieceTypes b);
 bool       PieceTypes_Resize(PieceTypes* dst, size_t len);
@@ -107,25 +116,28 @@ bool       Piece_Equals(Piece a, Piece b);
 bool       Move_Equals(Move a, Move b);
 bool       Move_IsValid(Move a);
 bool       MoveResult_Equals(const MoveResult* a, const MoveResult* b);
-Piece      Squares_At(const Squares s, Pos pos);
-void       Squares_UpdateAt(Squares s, Pos pos, Piece p);
+Piece      Squares_At(const Squares ss, Pos pos);
+void       Squares_UpdateAt(Squares ss, Pos pos, Piece p);
 void       Squares_Copy(Squares dst, const Squares src);
-bool       Squares_Equals(const Squares a, const Squares b);
+bool       Squares_Equals(const Squares ss, const Squares b);
 void       Squares_PlacePieces(Squares dst);
+bool       Squares_IsThreatened(const Squares ss, Pos pos);
+bool       Squares_CanKingMove(const Squares ss, Pos p);
+void       Squares_Move(Squares dst, Pos to, Pos from);
+MoveResult Squares_CheckMove(const Squares ss, Move m);
+MoveResult Squares_CheckPawnMove(const Squares ss, Move m);
+MoveResult Squares_CheckRookMove(const Squares ss, Move m);
+MoveResult Squares_CheckKnightMove(const Squares ss, Move m);
+MoveResult Squares_CheckBishopMove(const Squares ss, Move m);
+MoveResult Squares_CheckQueenMove(const Squares ss, Move m);
+MoveResult Squares_CheckKingMove(const Squares ss, Move m);
 bool       SideState_Equals(const SideState* a, SideState const* b);
 void       SideState_Copy(const SideState* src, SideState* dst);
 bool       Board_Equals(const Board* a, const Board* b);
 void       Board_Copy(Board* dst, const Board* src);
 MoveResult Board_MakeMove(Board* dst, Move m);
-MoveResult Board_CheckMove(const Board* b, Move m);
-MoveResult Board_CheckPawnMove(const Board* b, Move m);
-MoveResult Board_CheckRookMove(const Board* b, Move m);
-MoveResult Board_CheckKnightMove(const Board* b, Move m);
-MoveResult Board_CheckBishopMove(const Board* b, Move m);
-MoveResult Board_CheckQueenMove(const Board* b, Move m);
-MoveResult Board_CheckKingMove(const Board* b, Move m);
 bool       Threat_IsValid(Threat t);
-void       Threats_Append(Threats* ts, Threat t);
-void       Threats_Collect(Threats* dst, const Board* b, Pos p);
+void       Threats_Append(Threats* dst, Threat t);
+void       Threats_Collect(Threats* dst, const Squares ss, Pos p);
 
 #endif  // BOARD_H
